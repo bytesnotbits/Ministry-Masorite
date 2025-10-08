613cfffa262154939aeca14ac4dd407fa14b74ac
bytesnotbits <primextras32@gmail.com>
2025-10-07 21:13:00 -0500

    Give the onupgradeneeded function instructions on how to "move the furniture." For every territory that exists in the old database, we will automatically create a corresponding "default" street. Then, we will go through every single old house, find its new default street, and assign the correct streetId to it.

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 // --- DATABASE INITIALIZATION ---
 const DB_NAME = 'MinistryScribeDB';
-const DB_VERSION = 3; // Version is correct
+const DB_VERSION = 3;
 let db;
 
 function initDB() {
@@ -10,29 +10,30 @@ function initDB() {
         request.onupgradeneeded = (event) => {
             db = event.target.result;
             const transaction = event.target.transaction;
+            console.log(`Upgrading database from version ${event.oldVersion} to ${event.newVersion}`);
 
+            // --- SCHEMA MIGRATION ---
             // Create Territories store if it doesn't exist
             if (!db.objectStoreNames.contains('territories')) {
                 db.createObjectStore('territories', { keyPath: 'id', autoIncrement: true });
             }
 
             // Create Streets store if it doesn't exist
+            let streetsStore;
             if (!db.objectStoreNames.contains('streets')) {
-                const streetsStore = db.createObjectStore('streets', { keyPath: 'id', autoIncrement: true });
+                streetsStore = db.createObjectStore('streets', { keyPath: 'id', autoIncrement: true });
                 streetsStore.createIndex('territoryId', 'territoryId', { unique: false });
+            } else {
+                streetsStore = transaction.objectStore('streets');
             }
-            
+
             // Create or modify the Houses store
             let housesStore;
             if (db.objectStoreNames.contains('houses')) {
-                // If it exists, get a reference to it from the upgrade transaction
                 housesStore = transaction.objectStore('houses');
             } else {
-                // If it doesn't exist, create it
                 housesStore = db.createObjectStore('houses', { keyPath: 'id', autoIncrement: true });
             }
-
-            // Safely remove the old index and add the new one
             if (housesStore.indexNames.contains('territoryId')) {
                 housesStore.deleteIndex('territoryId');
             }
@@ -49,6 +50,67 @@ function initDB() {
                 const peopleStore = db.createObjectStore('people', { keyPath: 'id', autoIncrement: true });
                 peopleStore.createIndex('houseId', 'houseId', { unique: false });
             }
+
+            // --- DATA MIGRATION from v2 to v3 ---
+            // This block only runs if the user is coming from an older version.
+            if (event.oldVersion < 3) {
+                console.log("Performing data migration for houses...");
+                const territoriesStore = transaction.objectStore('territories');
+                
+                // We need to wait for territory data before we can migrate houses
+                territoriesStore.getAll().onsuccess = (e) => {
+                    const territories = e.target.result;
+                    const territoryMap = new Map(territories.map(t => [t.id, t]));
+                    const streetPromises = [];
+                    const newStreetMap = new Map(); // Maps old territoryId to new streetId
+
+                    // 1. Create a default street for each old territory
+                    for (const territory of territories) {
+                        const newStreet = {
+                            territoryId: territory.id,
+                            // The old `territory.name` was the street name. We use it to create the new street.
+                            name: territory.name || `Street for Territory #${territory.number}` 
+                        };
+                        const addRequest = streetsStore.add(newStreet);
+                        const promise = new Promise((res) => {
+                            addRequest.onsuccess = (event) => {
+                                // Store the newly created street's ID, linked to the old territory ID
+                                newStreetMap.set(territory.id, event.target.result);
+                                res();
+                            };
+                        });
+                        streetPromises.push(promise);
+                    }
+
+                    // 2. Once all new streets are created, update the houses
+                    Promise.all(streetPromises).then(() => {
+                        console.log("Default streets created. Updating houses...");
+                        const houseCursorRequest = housesStore.openCursor();
+                        houseCursorRequest.onsuccess = (event) => {
+                            const cursor = event.target.result;
+                            if (cursor) {
+                                const house = cursor.value;
+                                // Find the new streetId that corresponds to the house's old territoryId
+                                if (house.territoryId && newStreetMap.has(house.territoryId)) {
+                                    house.streetId = newStreetMap.get(house.territoryId);
+                                    delete house.territoryId; // Clean up the old property
+                                    cursor.update(house);
+                                }
+                                cursor.continue();
+                            } else {
+                                console.log("House data migration complete.");
+                            }
+                        };
+                    });
+                    
+                    // 3. Update the territory object to have a "description" instead of a "name"
+                    for(const territory of territories){
+                        territory.description = territory.name || 'General';
+                        delete territory.name;
+                        territoriesStore.put(territory);
+                    }
+                };
+            }
         };
 
         request.onsuccess = (event) => {
@@ -62,6 +124,7 @@ function initDB() {
         };
     });
 }
+
 
 // --- GENERIC CRUD FUNCTIONS (Unchanged) ---
 function addToStore(storeName, item) {
